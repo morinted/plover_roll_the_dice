4239d1a436c70774ac5ef73f0ef06d61ac1ff1cc
Ted Morin <morinted@gmail.com>
2021-04-15 10:06:21 -0400

    Fix dice range (d6 rolled 1 to 5)

--- roll_the_dice/dice.py
+++ roll_the_dice/dice.py
@@ -3,6 +3,6 @@ import random
 def dice(sides):
     if sides == 10:
         # Of course d10s have to be different.
-        return str(random.randrange(0, 9))
+        return str(random.randrange(0, 10))
     else:
-        return str(random.randrange(1, sides))
+        return str(random.randrange(1, sides + 1))
